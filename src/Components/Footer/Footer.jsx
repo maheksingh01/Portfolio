3fe4d725d3119ceeb907993d996e01cf42cea2b2
Mahek <mahek.2002.singh@gmail.com>
2024-08-09 08:12:23 +0530

    final commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Footer.css";
+import footer_logo from "../../assets/logo.png";
+// import footer_logo from "../../assets/footer_logo.svg";
+import user_icon from "../../assets/user_icon.svg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footer_logo} alt="" />
+          <p>
+            I am a frontend developer from Uttar Pradesh and I am a fresher who
+            is hard-working and passionate learner keen to learn new
+            technologies. I have various project in front end. I have skills in
+            HTML5, CSS3, Bootstrap, JavaScript and React JS
+          </p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          © 2024 Mahek Singh. All rights reserved.
+        </p>
+        <div className="footer-bottom-right">
+          <p>Terms Of Service</p>
+          <p>Privacy Policy</p>
+          <p>Connect with me</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,54 @@
+// import project1_img from "../assets/project_1.svg";
+// import project2_img from "../assets/project_2.svg";
+// import project3_img from "../assets/project_3.svg";
+// import project4_img from "../assets/project_4.svg";
+// import project5_img from "../assets/project_5.svg";
+// import project6_img from "../assets/project_6.svg";
+
+import project1_img from "../assets/project1.jpg";
+import project2_img from "../assets/project2.jpg";
+import project3_img from "../assets/project3.jpg";
+import project4_img from "../assets/project4.jpg";
+import project5_img from "../assets/project5.jpg";
+import project6_img from "../assets/project6.jpg";
+
+const mywork_data = [
+  {
+    w_no: 1,
+    w_name: "BIAS Hostel Complaint System",
+    w_img: project1_img,
+    w_url: "https://bias-hostel-complaint-system.onrender.com/",
+  },
+  {
+    w_no: 2,
+    w_name: "E-Commerce Website",
+    w_img: project2_img,
+    w_url: "https://maheksingh01.github.io/E-Commerce-Website/",
+  },
+  {
+    w_no: 3,
+    w_name: "Weather App",
+    w_img: project3_img,
+    w_url: "https://maheksingh01.github.io/weather_app/",
+  },
+  {
+    w_no: 4,
+    w_name: "Daily Quote",
+    w_img: project4_img,
+    w_url: "https://maheksingh01.github.io/daily-quote/",
+  },
+  {
+    w_no: 5,
+    w_name: "To-Do-List",
+    w_img: project5_img,
+    w_url: "https://maheksingh01.github.io/to-do-list/",
+  },
+  {
+    w_no: 6,
+    w_name: "Recipe Page",
+    w_img: project6_img,
+    w_url: "https://maheksingh01.github.io/recipe-page/",
+  },
+];
+
+export default mywork_data;
